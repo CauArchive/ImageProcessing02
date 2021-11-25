17681f5010f453b568acf9ca70e573b39b22241b
woodi <woodi.daily@gmail.com>
2021-11-25 13:46:17 +0900

    Add Sample Opencv Pre-Impl Code

--- IPO02/IPO02/main.cpp
+++ IPO02/IPO02/main.cpp
@@ -1,92 +1,124 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html
+
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/video.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
 #include <iostream>
-#include <string>
 
-#include "opencv2/opencv.hpp"
-
-using namespace cv;
 using namespace std;
+using namespace cv;
 
-void setLabel(Mat& image, string str, vector<Point> contour) {
-	int fontface = FONT_HERSHEY_SIMPLEX;
-	double scale = 0.5;
-	int thickness = 1;
-	int baseline = 0;
-	Size text = getTextSize(str, fontface, scale, thickness, &baseline);
-	Rect r = boundingRect(contour);
-	Point pt(r.x + ((r.width - text.width) / 2),
-		r.y + ((r.height + text.height) / 2));
-	rectangle(image, pt + Point(0, baseline),
-		pt + Point(text.width, -text.height), CV_RGB(200, 200, 200),
-		FILLED);
-	putText(image, str, pt, fontface, scale, CV_RGB(0, 0, 0), thickness, 8);
-}
+int main(int argc, const char** argv)
+{
+    const String keys = "{c camera     | 0 | use video stream from camera (device index starting from 0) }"
+        "{fn file_name |   | use video file as input }"
+        "{m method | mog2 | method: background subtraction algorithm ('knn', 'mog2')}"
+        "{h help | | show help message}";
+    CommandLineParser parser(argc, argv, keys);
+    parser.about("This sample demonstrates background segmentation.");
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
+    int camera = parser.get<int>("camera");
+    String file = parser.get<String>("file_name");
+    String method = parser.get<String>("method");
+    if (!parser.check())
+    {
+        parser.printErrors();
+        return 1;
+    }
 
-void convertColorToGray(Mat& image, Mat& gray) {
-	int rows = image.rows, cols = image.cols;
-	gray.create(image.size(), CV_8UC1);
-	if (image.isContinuous() && gray.isContinuous()) {
-		cols = rows * cols;
-		rows = 1;
-	}
-	for (int row = 0; row < rows; row++) {
-		uchar* pointer_row = image.ptr<uchar>(row);
-		uchar* pointer_row_gray = gray.ptr<uchar>(row);
-		for (int col = 0; col < cols; col++) {
-			pointer_row_gray[col] =
-				(uchar)(0.299f * pointer_row[0] + 0.587f * pointer_row[1] +
-					0.114f * pointer_row[2]);
-			pointer_row += 3;
-		}
-	}
-}
+    VideoCapture cap;
+    if (file.empty())
+        cap.open(camera);
+    else
+    {
+        file = samples::findFileOrKeep(file);  // ignore gstreamer pipelines
+        cap.open(file.c_str());
+    }
+    if (!cap.isOpened())
+    {
+        cout << "Can not open video stream: '" << (file.empty() ? "<camera>" : file) << "'" << endl;
+        return 2;
+    }
 
-void convertGrayToBinary(Mat& gray, Mat& binary, int thresholdValue, int maxValue) {
-	threshold(gray, binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
-}
+    Ptr<BackgroundSubtractor> model;
+    if (method == "knn")
+        model = createBackgroundSubtractorKNN();
+    else if (method == "mog2")
+        model = createBackgroundSubtractorMOG2();
+    if (!model)
+    {
+        cout << "Can not create background model using provided method: '" << method << "'" << endl;
+        return 3;
+    }
 
-void findContoursFromBinary(Mat& binary, vector<vector<Point> >& contours) {
-	findContours(binary, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
-}
+    cout << "Press <space> to toggle background model update" << endl;
+    cout << "Press 's' to toggle foreground mask smoothing" << endl;
+    cout << "Press ESC or 'q' to exit" << endl;
+    bool doUpdateModel = true;
+    bool doSmoothMask = false;
 
-int main(int, char**) {
-	Mat img, img_result, img_gray;
-	// Image Load
-	img = imread("2.png", IMREAD_COLOR);
-	if (img.empty()) {
-		cout << "No Image Found\n";
-		return -1;
-	}
-	// Convert Image to GrayScale
-	convertColorToGray(img, img_gray);
-	// Convert Image to Binary Image
-	Mat img_binary;
-	convertGrayToBinary(img_gray, img_binary, 30, 255);
-	// Find Contours of Images
-	vector<vector<Point> > contours;
-	findContoursFromBinary(img_binary, contours);
-	// Approximate Contours
-	vector<Point2f> approx;
-	img_result = img.clone();
-	for (size_t i = 0; i < contours.size(); i++) {
-		approxPolyDP(Mat(contours[i]), approx,
-			arcLength(Mat(contours[i]), true) * 0.02, true);
-		if (fabs(contourArea(Mat(approx))) < 120000)  // Detect Image with Area < 120000)
-		{
-			int size = approx.size();
-			// If approximated Contour size is over 3, it is a triangle
-			if (size == 3) setLabel(img_result, "triangle", contours[i]);
-			// If approximated Contour size is over 4, it is a rectangle
-			else if (size == 4 && isContourConvex(Mat(approx)))
-				setLabel(img_result, "rectangle", contours[i]);
-			else if (size <= 7 && isContourConvex(Mat(approx)))
-				setLabel(img_result, to_string(approx.size()), contours[i]);
-			// Else decide it is circle
-			else
-				setLabel(img_result, "circle", contours[i]);
-		}
-	}
-	imshow("input", img);
-	imshow("result", img_result);
-	waitKey(0);
-	return 0;
+    Mat inputFrame, frame, foregroundMask, foreground, background;
+    for (;;)
+    {
+        // prepare input frame
+        cap >> inputFrame;
+        if (inputFrame.empty())
+        {
+            cout << "Finished reading: empty frame" << endl;
+            break;
+        }
+        const Size scaledSize(640, 640 * inputFrame.rows / inputFrame.cols);
+        resize(inputFrame, frame, scaledSize, 0, 0, INTER_LINEAR);
+
+        // pass the frame to background model
+        model->apply(frame, foregroundMask, doUpdateModel ? -1 : 0);
+
+        // show processed frame
+        imshow("image", frame);
+
+        // show foreground image and mask (with optional smoothing)
+        if (doSmoothMask)
+        {
+            GaussianBlur(foregroundMask, foregroundMask, Size(11, 11), 3.5, 3.5);
+            threshold(foregroundMask, foregroundMask, 10, 255, THRESH_BINARY);
+        }
+        if (foreground.empty())
+            foreground.create(scaledSize, frame.type());
+        foreground = Scalar::all(0);
+        frame.copyTo(foreground, foregroundMask);
+        imshow("foreground mask", foregroundMask);
+        imshow("foreground image", foreground);
+
+        // show background image
+        model->getBackgroundImage(background);
+        if (!background.empty())
+            imshow("mean background image", background);
+
+        // interact with user
+        const char key = (char)waitKey(30);
+        if (key == 27 || key == 'q') // ESC
+        {
+            cout << "Exit requested" << endl;
+            break;
+        }
+        else if (key == ' ')
+        {
+            doUpdateModel = !doUpdateModel;
+            cout << "Toggle background update: " << (doUpdateModel ? "ON" : "OFF") << endl;
+        }
+        else if (key == 's')
+        {
+            doSmoothMask = !doSmoothMask;
+            cout << "Toggle foreground mask smoothing: " << (doSmoothMask ? "ON" : "OFF") << endl;
+        }
+    }
+    return 0;
 }
